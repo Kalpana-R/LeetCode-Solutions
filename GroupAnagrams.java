42d2787b3a91594f8dd9d0011b04ccbb5f3a03e7
kalpana-R <kalpanaraj1105@gmail.com>
2023-08-01 09:30:58 +0530

    beates 97%

---
+++ GroupAnagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+     
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(int i=0;i<strs.length;i++)
+        {
+
+           char[] c = strs[i].toCharArray();
+           Arrays.sort(c);
+           String s = new String(c);
+         if(!map.containsKey(s))
+        map.put(s, new ArrayList<String>());
+         
+            map.get(s).add(strs[i]);
+
+           }
+       
+       return map.values().stream().collect(Collectors.toList());
+    }
+}
